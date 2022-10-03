688312000213e69321365c3b72071c9b90c32818
jordipr <jordi_lleida@alumnes.ilernalleida.com>
2022-10-03 16:37:25 +0200

    ejercicio_unity

--- Actividad_1/Assets/scripts/MoverObjetos.cs
+++ Actividad_1/Assets/scripts/MoverObjetos.cs
@@ -21,43 +21,141 @@ public class MoverObjetos : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown("z"))
-        {
+    void Update(){
 
-            objetos[0].transform.position += Vector3.right;
+      
+            
 
+            if (Input.GetKeyDown("z"))
+            {
 
-        }else if (Input.GetKeyDown("x"))
-        {
-
-
-            objetos[1].transform.position += Vector3.right;
+                objetos[0].transform.position += Vector3.right;
 
 
+            }
+            else if (Input.GetKeyDown("x"))
+            {
 
-        }else if (Input.GetKeyDown("c"))
-        {
-            objetos[2].transform.position += Vector3.right;
-        
-        
-        }else if (Input.GetKeyDown("v"))
-        {
 
-            objetos[3].transform.position += Vector3.right;
+                objetos[1].transform.position += Vector3.right;
 
 
 
-        }else if (Input.GetKeyDown("b"))
-        {
-
-            objetos[4].transform.position += Vector3.right;
-
-
-        }
+            }
+            else if (Input.GetKeyDown("c"))
+            {
+                objetos[2].transform.position += Vector3.right;
 
 
-        
+            }
+            else if (Input.GetKeyDown("v"))
+            {
+
+                objetos[3].transform.position += Vector3.right;
+
+
+
+            }
+            else if (Input.GetKeyDown("b"))
+            {
+
+                objetos[4].transform.position += Vector3.right;
+
+
+            }
+            else if (Input.GetKeyDown("q"))
+            {
+
+                objetos[0].transform.position += Vector3.left;
+
+
+            }
+            else if (Input.GetKeyDown("w"))
+            {
+
+                objetos[1].transform.position += Vector3.left;
+
+
+            }
+            else if (Input.GetKeyDown("e"))
+            {
+
+                objetos[2].transform.position += Vector3.left;
+
+
+            }
+            else if (Input.GetKeyDown("r"))
+            {
+
+                objetos[3].transform.position += Vector3.left;
+
+
+            }
+            else if (Input.GetKeyDown("t"))
+            {
+
+                objetos[4].transform.position += Vector3.left;
+
+
+            }
+            else if (Input.GetKeyDown("a"))
+            {
+
+                objetos[0].transform.position += Vector3.up;
+
+
+            }
+            else if (Input.GetKeyDown("s"))
+            {
+
+                objetos[1].transform.position += Vector3.up;
+
+
+            }
+            else if (Input.GetKeyDown("d"))
+            {
+
+                objetos[2].transform.position += Vector3.up;
+
+
+            }
+            else if (Input.GetKeyDown("f"))
+            {
+
+                objetos[3].transform.position += Vector3.up;
+
+
+            }
+            else if (Input.GetKeyDown("g"))
+            {
+
+                objetos[4].transform.position += Vector3.up;
+
+
+            }
+            else if (Input.GetKeyDown("space"))
+            {
+
+                objetos[0].transform.position = Vector3.zero;
+                objetos[1].transform.position = Vector3.zero;
+                objetos[2].transform.position = Vector3.zero;
+                objetos[3].transform.position = Vector3.zero;
+                objetos[4].transform.position = Vector3.zero;
+
+            }else if (objetos[3].transform.position.x > 4 || objetos[3].transform.position.x < -4){
+
+            objetos[3].transform.position = Vector3.zero;
+
+
+            }else if (objetos[4].transform.position.x > 4 || objetos[4].transform.position.x < -4){
+
+
+            objetos[4].transform.position = Vector3.zero;
+
+
+            }
+
     }
-}
+
+    }
+
